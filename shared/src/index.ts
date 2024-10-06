f3f2462d7dc4c18b86f576a15c4b06838fd4ca8e
mikkelklitlund <mik.kkel@hotmail.com>
2024-10-06 20:06:05 +0200

    added shared models

--- shared/src/index.ts
+++ shared/src/index.ts
@@ -1,4 +1,95 @@
-export interface MyModel {
-    id: string;
-    name: string
+export interface Company {
+    id: number;
+    address: string;
+    name: string;
+    employees: Employee[];
+    department: Department[];
+    employeeTypes: EmployeeType[];
+    users: UserCompanyAccess[];
+}
+
+export interface User {
+    id: number;
+    email: string;
+    password: string;
+    username: string;
+    companyAccesses: UserCompanyAccess[];
+}
+
+export interface UserCompanyAccess {
+    userId: number;
+    companyId: number;
+    role: Role;
+}
+
+export interface Department {
+    id: number;
+    name: string;
+    companyId: number;
+    employees: Employee[];
+}
+
+export interface Employee {
+    id: number;
+    name: string;
+    profilePicturePath: string;
+    companyId: number;
+    departmentId: number;
+    checkedIn: boolean;
+    birthday: Date;
+    employeeTypeId: number;
+    monthlySalary?: number;
+    hourlySalary?: number;
+    address: string;
+    city: string;
+    absenceRecords?: AbsenceRecord[];
+    attendanceRecords?: AttendanceRecord[];
+}
+
+export interface CreateEmployee {
+    name: string;
+    profilePicturePath: string;
+    companyId: number;
+    departmentId: number;
+    checkedIn: boolean;
+    birthday: Date;
+    employeeTypeId: number;
+    monthlySalary?: number;
+    hourlySalary?: number;
+    address: string;
+    city: string;
+}
+
+export interface EmployeeType {
+    id: number;
+    name: string;
+    companyId: number;
+}
+
+export interface AbsenceRecord {
+    id: number;
+    employeeId: number;
+    startDate: Date;
+    endDate?: Date;
+    absenceType: AbsenceType;
+}
+
+export interface AttendanceRecord {
+    id: number;
+    employeeId: number;
+    date: Date;
+    checkIn: Date;
+    checkOut?: Date;
+}
+
+export enum AbsenceType {
+    VACATION,
+    SICK,
+    HOMEDAY,
+    PUBLIC_HOLIDAY
+}
+
+export enum Role {
+    ADMIN,
+    USER
 }
